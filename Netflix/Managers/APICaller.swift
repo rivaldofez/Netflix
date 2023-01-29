06e44c2682bcd183baec4778401de62c4cd60772
rivaldofez <rivaldofez@gmail.com>
2023-01-29 21:53:34 +0700

    add completion handle for movies in homeview controller

--- Netflix/Managers/APICaller.swift
+++ Netflix/Managers/APICaller.swift
@@ -12,11 +12,14 @@ struct Constants {
     static let baseURL = "https://api.themoviedb.org"
 }
 
+enum APIError: Error {
+    case failedToGetData
+}
 
 class APICaller {
     static let shared = APICaller()
     
-    func getTrendingMovies(completion: @escaping (String) -> Void){
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void){
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else { return }
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
@@ -27,9 +30,9 @@ class APICaller {
             do {
 //                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
                 let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
-                print(results.results[0].original_title)
+                completion(.success(results.results))
             } catch {
-                print(error.localizedDescription)
+                completion(.failure(error))
             }
         }
         
