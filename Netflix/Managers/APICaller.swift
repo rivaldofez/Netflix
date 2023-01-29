0870d0861d79de33760ca5de4b8c079c707c1dd9
rivaldofez <rivaldofez@gmail.com>
2023-01-29 22:34:24 +0700

    add api caller for upcoming movies

--- Netflix/Managers/APICaller.swift
+++ Netflix/Managers/APICaller.swift
@@ -48,8 +48,9 @@ class APICaller {
             }
             
             do {
-                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                let results = try JSONDecoder().decode(TrendingTvsResponse.self, from: data)
                 print(results)
+                
             }catch{
                 print(error.localizedDescription)
             }
@@ -59,4 +60,24 @@ class APICaller {
         
     }
     
+    func getUpcomingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/movie/upcoming?api_key=\(Constants.API_KEY)&language=en-US&page=1") else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                print(results)
+            }catch{
+                print(error.localizedDescription)
+            }
+        }
+        
+    }
+    
 }
+
+//https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1
