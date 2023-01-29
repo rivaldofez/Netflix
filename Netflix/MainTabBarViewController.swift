4696046505b1a5733e34ad59dbfce5598d86f396
rivaldofez <rivaldofez@gmail.com>
2023-01-29 15:52:31 +0700

    set tab item image

--- Netflix/MainTabBarViewController.swift
+++ Netflix/MainTabBarViewController.swift
@@ -13,6 +13,22 @@ class MainTabBarViewController: UITabBarController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
+        
+        let homeVC = UINavigationController(rootViewController: HomeViewController())
+        
+        let upcomingVC = UINavigationController(rootViewController: UpcomingViewController())
+        
+        let searchVC = UINavigationController(rootViewController: SearchViewController())
+        
+        let downloadVC = UINavigationController(rootViewController: DownloadViewController())
+        
+        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        upcomingVC.tabBarItem.image = UIImage(systemName: "play.circle")
+        searchVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        downloadVC.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        
+        setViewControllers([homeVC, upcomingVC, searchVC, downloadVC], animated: true)
     }
 
 
