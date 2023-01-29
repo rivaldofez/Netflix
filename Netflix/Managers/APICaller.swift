bb74ead4d421884b211baedc3c78937fe64b0cc0
rivaldofez <rivaldofez@gmail.com>
2023-01-29 22:38:41 +0700

    add api caller for popular movies

--- Netflix/Managers/APICaller.swift
+++ Netflix/Managers/APICaller.swift
@@ -76,8 +76,31 @@ class APICaller {
             }
         }
         
+        task.resume()
+        
+    }
+    
+    func getPopularMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/movie/popular?api_key=\(Constants.API_KEY)&language=en-US&page=1") else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                print(results)
+            }catch{
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
+        
     }
     
 }
 
 //https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1
+//https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
