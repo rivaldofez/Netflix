9a342a97a6cd027d922957f5ef4f7bcbd9efaffd
rivaldofez <rivaldofez@gmail.com>
2023-01-29 22:10:27 +0700

    add api caller for tv trending

--- Netflix/Managers/APICaller.swift
+++ Netflix/Managers/APICaller.swift
@@ -20,7 +20,7 @@ class APICaller {
     static let shared = APICaller()
     
     func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void){
-        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else { return }
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/movie/day?api_key=\(Constants.API_KEY)") else { return }
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else {
@@ -38,4 +38,25 @@ class APICaller {
         
         task.resume()
     }
+    
+    func getTrendingTvs(completion: @escaping (Result<[Movie], Error>) -> Void){
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/tv/day?api_key=\(Constants.API_KEY)") else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                print(results)
+            }catch{
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
+        
+    }
+    
 }
