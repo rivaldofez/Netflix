55be9cced10b466402013f627d185800b30ccf2d
rivaldofez <rivaldofez@gmail.com>
2023-01-30 21:19:02 +0700

    add model for youtube response

--- Netflix/Model/YoutubeSearchResponse.swift
+++ Netflix/Model/YoutubeSearchResponse.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct YoutubeSearchResponse: Codable {
+    let items: [VideoItem]
+}
+
+struct VideoItem: Codable {
+    let id: VideoElement
+}
+
+struct VideoElement: Codable {
+    let kind: String
+    let videoId: String
+}
