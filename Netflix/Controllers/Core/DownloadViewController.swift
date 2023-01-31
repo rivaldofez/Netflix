ca686c4c8c832c66bc766d9ef19dc5ef57672497
rivaldofez <rivaldofez@gmail.com>
2023-01-31 15:17:43 +0700

    add table view to download page controller

--- Netflix/Controllers/Core/DownloadViewController.swift
+++ Netflix/Controllers/Core/DownloadViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class DownloadViewController: UIViewController {
+    
+    private var films : [FilmItem] = [FilmItem]()
+    
+    private let downloadTable : UITableView = {
+        let table = UITableView()
+        table.register(FilmTableViewCell.self, forCellReuseIdentifier: FilmTableViewCell.identifier)
+        return table
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        title = "Download"
+        
+        
+        downloadTable.delegate = self
+        downloadTable.dataSource = self
     }
 
 
+}
+
+
+extension DownloadViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return films.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FilmTableViewCell.identifier, for: indexPath) as? FilmTableViewCell else { return UITableViewCell()}
+        
+        let film = films[indexPath.row]
+        
+        cell.configure(with: FilmViewModel(filmName: (film.original_name ?? film.original_title) ?? "" , posterURL: film.poster_path ?? ""))
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
 }
