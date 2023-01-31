5bf17b51aff663fefe7cdc86aab07365a08a9390
rivaldofez <rivaldofez@gmail.com>
2023-01-31 15:06:02 +0700

    add function to save data film to core data

--- Netflix/Managers/DataPersistenceManager.swift
+++ Netflix/Managers/DataPersistenceManager.swift
@@ -5,4 +5,34 @@
 //  Created by Rivaldo Fernandes on 31/01/23.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+
+class DataPersistenceManager {
+    static let shared = DataPersistenceManager()
+    
+    func downloadFilm(model: Film, completion: @escaping (Result<Void, Error>) -> Void){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let item = FilmItem(context: context)
+        
+        item.original_title = model.original_title
+        item.original_name = model.original_name
+        item.overview = model.overview
+        item.poster_path = model.poster_path
+        item.id = Int64(model.id)
+        item.media_type = model.media_type
+        item.vote_average = model.vote_average
+        item.vote_count = Int64(model.vote_count)
+        item.release_date = model.release_data
+        
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
